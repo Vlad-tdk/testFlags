735c53ad09eb8a4745725fea709ed20e185ed364
Vlad-tdk <xalif80@gmail.com>
2024-04-01 21:07:58 +0200

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Vlad on 1.4.24..
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
